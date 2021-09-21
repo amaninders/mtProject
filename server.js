55c9d2de03f16d236b4a3caf34426a58a30200e1
Calvin Xu <calvin.xu.jy@gmail.com>
2021-09-21 23:00:17 +0000

    Add route, controller, form POST method for login

--- server.js
+++ server.js
@@ -39,6 +39,7 @@ app.use(express.static("public"));
 const usersRoutes = require("./routes/users");
 const mapsRoutes = require("./routes/maps");
 const favsRoutes = require("./routes/favs");
+const loginRoute = require("./routes/login");
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
@@ -46,6 +47,7 @@ app.use("/api/users", usersRoutes(db));
 // Note: mount other resources here, using the same pattern above
 app.use("/api/maps", mapsRoutes(db));
 app.use("/api/favs", favsRoutes(db));
+app.use("/login", loginRoute(db));
 
 // Home page
 // Warning: avoid creating more routes in this file!
