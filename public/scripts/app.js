f69deeb8fbdcd55bedda7de43fce7889f50b7c30
amaninders <amandeolofficial@gmail.com>
2021-09-23 13:13:55 +0000

    completed the map creation workflow successfully, now just need to finish the listener for  marker addition

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -91,7 +91,62 @@ $("body").on('submit', event => {
         $('#currentMap__container').empty();
         $('#currentMap__container').append(currentMap(result));
         $('#currentMap').addClass('show');
-        // Initialize and add the map ##### to-be-continued #####
+        const pos = {
+          lat: result.latitude,
+          lng: result.longitude,
+        };
+        map.setCenter(pos);
+        infoWindow.setPosition(pos);
+        infoWindow.setContent('Click the map to add a marker!&nbsp;&nbsp;');
+        map.setCenter(pos);
+
+        // Configure the click listener.
+  		  map.addListener("click", (mapsMouseEvent) => {
+  			const lat = mapsMouseEvent.latLng.toJSON().lat;
+  			const long = mapsMouseEvent.latLng.toJSON().lng;
+  			const contentString = `
+    			<form>
+    				<fieldset>
+    					<div class="form-group">
+    						<label for="latitude">Latitude</label>
+    						<input type="text" id="latitude" class="form-control" placeholder="${lat}" disabled>
+    					</div>
+    					<div class="form-group">
+    						<label for="longitude">Longitude</label>
+    						<input type="text" id="longitude" class="form-control" placeholder="${long}" disabled>
+    					</div>
+    					<div class="form-group">
+    						<label for="exampleFormControlSelect1">Marker Type</label>
+    						<select class="form-control" id="exampleFormControlSelect1">
+    							<option>Park</option>
+    							<option>Hotel</option>
+    							<option>Museum</option>
+    							<option>Gallery</option>
+    							<option>Shop</option>
+    							<option>Other</option>
+    						</select>
+    					</div>
+    					<div class="form-group">
+    						<label for="exampleFormControlTextarea1">Notes</label>
+    						<textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+    					</div>
+    					<button type="submit" class="btn btn-primary">Submit</button>
+    				</fieldset>
+    			</form>`;
+
+
+
+  			// Close the current InfoWindow.
+  			infoWindow.close();
+  			// Create a new InfoWindow.
+  			infoWindow = new google.maps.InfoWindow({
+  				position: mapsMouseEvent.latLng,
+  			});
+  			infoWindow.setContent(
+  				contentString
+  			);
+  			infoWindow.open(map);
+  	  	});
       })
       break;
     default:
